3f9151216357ed12fc3425136e428f7bf080118a
Alec Dye <alecdye@gmail.com>
2020-01-31 08:38:24 -0800

    passing props into character card and rendering correct data

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  // console.log(props)
+  return (
+    <div>
+      <h3>{props.character}</h3>
+      <p>{props.status}</p>
+      <p>{props.species}</p>
+    </div>
+  );
 }
