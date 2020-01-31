9e915ce642fd18cc8a42b5b1b379cab8abcc5ae1
Alec Dye <alecdye@gmail.com>
2020-01-31 08:19:00 -0800

    building list component

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
-export default function CharacterList() {
+export default function CharacterList(props) {
   // TODO: Add useState to track data from useEffect
-
+  const [characters, setCharacter] = useState([])
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
-
+    const getCharacters = () => {
+      axios
+        .get("https://rickandmortyapi.com/api/character/")
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log("Request error", error)
+        })
+    }
+    getCharacters();
+  }, [characters]);
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => (
+        <div>
+          <CharacterCard key={character.id} character={character} />
+        </div>
+      ))}
     </section>
   );
 }
