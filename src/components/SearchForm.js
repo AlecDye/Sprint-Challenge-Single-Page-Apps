8d69d6dce7dcbd322213e7e3474dd9a3edac5147
Alec Dye <alecdye@gmail.com>
2020-01-31 09:47:54 -0800

    prototyping functionality for search bar

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,21 +3,40 @@ import axios from "axios";
 
 export default function SearchForm() {
   const [searchWord, setSearchWord] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const handleInputChange = e => {
+    setSearchWord(e.target.value);
+  }
+
   useEffect(() => {
     axios
       .get(`https://rickandmortyapi.com/api/character/`)
       .then(response => {
-        console.log("Search", response)
+        // console.log("Search", response)
+        const results = response.data.results.filter(character => character.name.toLowerCase().includes(searchWord)
+        );
+        setSearchResults(results)
+      })
+      .catch(error => {
+        console.log("Search request error", error)
       })
   }, [searchWord]);
-  const handleInputChange = e => {
-    setSearchWord(e.target.value);
-  }
+
   return (
-    <section className="search-form">
-      <form className="searchBar">
-        <input type="text" onChange={handleInputChange} value={searchWord} name="name" />
-      </form>
-    </section>
+    <div>
+      <section className="search-form">
+        <form className="searchBar">
+          <input type="text" onChange={handleInputChange} value={searchWord} name="name" />
+        </form>
+      </section>
+      {searchResults.map(entry => (
+        <ul>
+          <li>{entry.name}</li>
+          <li>{entry.status}</li>
+          <li>{entry.speices}</li>
+        </ul>
+      ))}
+
+    </div>
   );
 }
